3d5478ba915d5001ca1ac4f385f4a2d1d383cef4
Kaito Udagawa <umireon@gmail.com>
2021-07-04 15:21:33 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+brew install --cask android-file-transfer
+brew install --cask audacity
+brew install --cask discord
+brew install --cask docker
+brew install --cask firefox
+brew install --cask fork
+brew install --cask github
+brew install --cask google-chrome
+brew install --cask google-cloud-sdk
+brew install --cask google-drive
+brew install --cask keka
+brew install --cask netgear-switch-discovery-tool
+brew install --cask obs
+brew install --cask plantronics-hub
+brew install --cask qfinder-pro
+brew install --cask qudedup-extract-tool
+brew install --cask vagrant
+brew install --cask virtualbox
+brew install --cask visual-studio-code
+brew install --cask wireshark
+
+brew install dash
+brew install ffmpeg
+brew install go
+brew install mp3gain
+brew install nmap
+brew install python@3.8
+brew install python@3.9
+brew install shellcheck
+brew install zsh-completions
